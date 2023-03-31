2b0f9d3051e6b35db878e514dfc07e2571e3ef7a
Shivam8100 <shivamranjan81@gmail.com>
2023-04-01 00:06:12 +0530

    came again to commit 1 using cherry-pick

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,12 @@ function App() {
   return (
     <div className="App">
       <h1>Hello there i am writting this line for commit first sentense</h1>
+<<<<<<< HEAD
       <h1>This is the second commit</h1>
       <h1>this is second commit</h1>
       {/* //now using  git merge --squashcommand */}
+=======
+>>>>>>> 1c34215 (adding first commit)
      
     </div>
   )
