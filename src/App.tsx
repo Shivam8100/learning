312df8c1a221e9816d3f0f957a35e03f8d262489
Shivam8100 <shivamranjan81@gmail.com>
2023-04-01 00:23:02 +0530

    git merge --squash used for backup from prev command

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello there i am writting this line for commit first sentense</h1>
+      <h1>This is the second commit</h1>
+      <h1>this is second commit</h1>
+      {/* //now using  git merge --squashcommand */}
      
     </div>
   )
