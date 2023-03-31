22fce42fc3e9f0e35e5df4be99c5b9d166b00a46
Shivam8100 <shivamranjan81@gmail.com>
2023-04-01 00:14:06 +0530

    added third commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <h1>Hello there i am writting this line for commit first sentense</h1>
       <h1>This is the second commit</h1>
+      <h1>this is second commit</h1>
      
     </div>
   )
