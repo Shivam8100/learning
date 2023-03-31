1c342155ee90d65d61f509ef815a2cba09852784
Shivam8100 <shivamranjan81@gmail.com>
2023-04-01 00:06:12 +0530

    adding first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hello there i am writting this line for commit first sentense</h1>
+     
+    </div>
+  )
+}
+
+export default App
