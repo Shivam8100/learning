f303a28609fc32005503082d37d03fe223f14944
Shivam8100 <shivamranjan81@gmail.com>
2023-04-01 00:12:41 +0530

    commit for using cherry-pick

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,7 @@ function App() {
       <h1>Hello there i am writting this line for commit first sentense</h1>
       <h1>This is the second commit</h1>
       <h1>this is second commit</h1>
-<<<<<<< HEAD
       {/* //now using  git merge --squashcommand */}
-=======
->>>>>>> 22fce42fc3e9f0e35e5df4be99c5b9d166b00a46
      
     </div>
   )
