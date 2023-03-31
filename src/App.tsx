b2104a2d7be0ae41de270d07b3555514cbd96724
Shivam8100 <shivamranjan81@gmail.com>
2023-04-01 00:12:41 +0530

    this is second commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello there i am writting this line for commit first sentense</h1>
+      <h1>This is the second commit</h1>
      
     </div>
   )
